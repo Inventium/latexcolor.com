7f7a1958ae6b4480bdaf61ef6bd846a214404fde
Dave Doolin <david.doolin@gmail.com>
2011-11-27 23:17:49 -0800

    added css deployment back in to rsync

--- deploy.sh
+++ deploy.sh
@@ -6,3 +6,4 @@ rsync -essh -vta --cvs-exclude index.html tinoboxc@tinobox.com:public_html/latex
 rsync -essh -vta --cvs-exclude sitemap.xml tinoboxc@tinobox.com:public_html/latexcolor
 rsync -essh -vta --cvs-exclude robots.txt tinoboxc@tinobox.com:public_html/latexcolor
 rsync -essh -vta --cvs-exclude js tinoboxc@tinobox.com:public_html/latexcolor/
+rsync -essh -vta --cvs-exclude css tinoboxc@tinobox.com:public_html/latexcolor/
