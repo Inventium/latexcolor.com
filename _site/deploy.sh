070848d3e537730152c082c40ef8229698714f92
Dave Doolin <david.doolin@gmail.com>
2017-04-11 08:52:17 -0700

    updated ruby and gems

--- _site/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo rsync deployment for latexcolor.com
-
-rsync -essh -vta --cvs-exclude index.html tinoboxc@tinobox.com:public_html/latexcolor
-rsync -essh -vta --cvs-exclude sitemap.xml tinoboxc@tinobox.com:public_html/latexcolor
-rsync -essh -vta --cvs-exclude css tinoboxc@tinobox.com:public_html/latexcolor/
