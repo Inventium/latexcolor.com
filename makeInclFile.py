6baf8158ddef757d035fd6de0d8e94679ebea289
Gilbert <foreachthing@users.noreply.github.com>
2018-09-19 13:53:58 +0200

    Package!
    Now the scripts creates two files:
    * `LatexColors.incl.sty` to be used as a package (`\usepackage{LatexColors.incl}` and then `\tcolor{asparagus}` becomes available).
    * and `LatexColors.incl.cwl` for TexStudio. This will list all the colors when start typing `\tcolor{`.

--- makeInclFile.py
+++ makeInclFile.py
@@ -8,27 +8,37 @@ import re # regex
 
 # define files
 dir_path = Path(__file__).resolve().parent
-outputfile = open(dir_path/'LatexColors.incl.tex','w')
+outputfile = open(dir_path/'LatexColors.incl.sty','w')
+cwloutput = open(dir_path/'LatexColors.incl.cwl','w')
+
 
 #what I dont want
-bad_chars = r"[\(\)\{\}\<\>\s#'\\]"
+bad_chars = r"[\(\)\{\}\<\>\s#'\\/]"
 
 def main():
-    try:   
+    try:
         ColorFile = open('colors.txt', 'r')
         colorlines = ColorFile.readlines()
         ColorFile.close()
 
-        outputfile.write('% Input in your TEX project and use color names accoringly.\n')
+        outputfile.write('% --- Usage ---\n')
+        outputfile.write('% Write in your premable:\n')
+        outputfile.write('% \\usepackage{LatexColors.incl}\n\n')
+        outputfile.write('% Then use the colors in your document like this: {\\tcolor{airforceblue} This is Air Force Blue}\n\n')
+
+        outputfile.write('\\NeedsTeXFormat{LaTeX2e}[1994/06/01]\n')
+        outputfile.write('\\ProvidesPackage{LatexColors.incl}[2018/9/11 ver 1.0]\n')
+        outputfile.write('\\PassOptionsToPackage{usenames,dvipsnames}{xcolor}\n\n')
+        outputfile.write('\\newcommand{\\tcolor}[1]{\\color{#1}}\n\n')
         outputfile.write('% Colors from: https://github.com/Inventium/latexcolor.com\n\n')
-        outputfile.write('% Usage:\n% Required Package:\n% \\RequirePackage[usenames,dvipsnames]{xcolor}\n% Input Colors:\n% \\input{LatexColors.incl.tex} \n\n')
-        outputfile.write('% Then use the colors in your document like this: {\\color{airforceblue} This is Air Force Blue}\n\n')
-        
+
+        cwloutput.write('# LatexColors.incl package\n#\\tcolor{keyvals}\n#keyvals:\\tcolor\n')
+
         for strLine in colorlines[1:]:
-        
+
             if len(strLine.strip()) > 0:
                 ln = strLine.split('\t')
-        
+
                 # get color name and hex
                 colorname = unidecode.unidecode(ln[0])
                 colorname = re.sub(bad_chars, '', colorname, 0, re.MULTILINE | re.IGNORECASE)
@@ -38,17 +48,24 @@ def main():
                 # \definecolor{airforceblue}{HTML}{5d8aa8}
                 outputfile.write('\\definecolor{' + colorname + '}{HTML}{' + colorhex + '}	\n')
 
+                cwloutput.write(colorname + '\n')
+
         outputfile.write('\\endinput')
+        cwloutput.write('#endkeyvals')
 
     except OSError as err:
-        print("OS error: {0}".format(err))
-    except:
-        print('Somme silly error has occured: ' + str(sys.exc_info()[0]))
+        print('OS error: {0} in {1}\n{2}'.format(err, err.filename, err.message))
+    except Exception as ex:
+        print (str(ex))
         outputfile.close()
+        cwloutput.close()
     else:
         print('All done.')
         outputfile.close()
-    
+        cwloutput.close()
+
+
+
 
 
 main()
